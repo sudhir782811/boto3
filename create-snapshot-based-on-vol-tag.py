b842f4d4a089f5e5c9f66a90a6e24602884464aa
sudhir <sudhir@IT000483.local>
2020-05-06 12:54:18 +0530

    1

--- create-snapshot-based-on-vol-tag.py
+++ create-snapshot-based-on-vol-tag.py
@@ -5,14 +5,14 @@ ec2_client = session.client(service_name="ec2", region_name="eu-central-1")
 
 prod_vol = {"Name": 'tag:ENV', 'Values': ['Prod']}
 response = ec2_client.describe_volumes(Filters=[prod_vol])
-my_vol_ids = []
+my_vol_id = []
 for i in response['Volumes']:
-    my_vol_ids.append(i['VolumeId'])
-    print('going to take snapshot of volumes: {}'.format(my_vol_ids))
+    my_vol_id.append(i['VolumeId'])
+    print('going to take snapshot of volumes: {}'.format(my_vol_id))
 
     response = ec2_client.create_snapshot(
         Description="boto3" + i['VolumeId'],
        VolumeId=i['VolumeId']
     )
 
-    print('snapshot has been done of vol: {}'.format(my_vol_ids))
+    print('snapshot has been done of vol: {}'.format(my_vol_id))
